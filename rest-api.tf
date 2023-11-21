9b232947f11a6e41c405603e726f726e385118c5
soyoon <soyoon.dev@gmail.com>
2023-11-21 10:28:10 +0900

    add profile methods

--- rest-api.tf
+++ rest-api.tf
@@ -82,3 +82,41 @@ resource "aws_api_gateway_resource" "profile_subresource" {
   parent_id   = aws_api_gateway_resource.user_resource.id
   path_part   = "profile"
 }
+
+resource "aws_api_gateway_method" "get_profile_method" {
+  rest_api_id = aws_api_gateway_rest_api.main_api.id
+  resource_id = aws_api_gateway_resource.profile_subresource.id
+  http_method = "GET"
+
+  authorization = "CUSTOM"
+  authorizer_id = aws_api_gateway_authorizer.hwahae_authorizer.id
+
+  request_parameters = {
+    "method.request.header.hwahae-platform"       = true
+    "method.request.header.hwahae-app-HWAHAE_SERVER_API_VERSION"    = true
+    "method.request.querystring.userId"           = true
+  }
+
+  request_models = {
+    "application/json" = "Empty"  # Adjust the content type and schema as needed
+  }
+
+}
+
+resource "aws_api_gateway_integration" "get_profile_integration" {
+  rest_api_id             = aws_api_gateway_rest_api.main_api.id
+  resource_id             = aws_api_gateway_resource.profile_subresource.id
+  http_method             = aws_api_gateway_method.get_profile_method.http_method
+  integration_http_method = "GET"
+  type                    = "HTTP_PROXY"
+
+  uri = "http://${var.HWAHAE_SERVER_API_ALB}/${var.HWAHAE_SERVER_API_VERSION}/users/profile"
+
+  request_parameters = {
+    "integration.request.header.hwahae-platform"      = "method.request.header.hwahae-platform"
+    "integration.request.header.hwahae-app-HWAHAE_SERVER_API_VERSION"   = "method.request.header.hwahae-app-HWAHAE_SERVER_API_VERSION"
+    "integration.request.querystring.userId"          = "method.request.querystring.userId"
+  }
+
+  passthrough_behavior = "WHEN_NO_MATCH"
+}
